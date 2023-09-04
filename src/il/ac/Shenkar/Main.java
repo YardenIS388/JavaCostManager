415c3b7f433fc65b5eb5596142e57cc5bba45073
yarden <yard48@gmail.com>
2023-09-04 21:04:28 +0300

    derby db

--- src/il/ac/Shenkar/Main.java
+++ src/il/ac/Shenkar/Main.java
@@ -1,11 +1,14 @@
 package il.ac.Shenkar;
 
+import il.ac.Shenkar.CostManager.Model.SimpleApp;
 import il.ac.Shenkar.CostManager.View.MainApp;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        new SimpleApp().go(args);
+        System.out.println("SimpleApp finished");
         System.out.println("start");
        MainApp mainGui = new MainApp();
        mainGui.start();
